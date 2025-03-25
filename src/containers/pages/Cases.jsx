c1b37277b26e7eaadd3b6d25d256388a4d8fd31c
Jorge Agustin Loyola <agusloyola21@gmail.com>
2025-03-25 13:35:23 -0300

    comenzamos con la pagina de cases

--- src/containers/pages/Cases.jsx
+++ src/containers/pages/Cases.jsx
@@ -1,12 +1,13 @@
 import Navbar from "components/navigation/Navbar"
 import Footer from "components/navigation/Footer"
 import Layout from "hocs/layouts/Layout"
+import Hero from "components/Cases/Hero"
 function Cases(){
     return(
         <Layout>
-            <Navbar/>
-            <div className="py-2 px-8 mx-14" style={{ marginTop: "75px" }} >
-                Cases
+            <div className=""  >
+                <Navbar/>
+                <Hero />
 
             </div>
             <Footer/>
