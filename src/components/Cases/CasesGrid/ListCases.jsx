7d6e7665c6b75828c4369d9768ca4dfc35f4863d
Jorge Agustin Loyola <agusloyola21@gmail.com>
2025-03-27 21:58:53 -0300

    Solucionamos el funcionamiento de las cardCase con useref de react

--- src/components/Cases/CasesGrid/ListCases.jsx
+++ src/components/Cases/CasesGrid/ListCases.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import cases from './ArrayCase'
+import CardCase from './CardCase'
 
 function ListCases() {
     return (
@@ -9,64 +10,23 @@ function ListCases() {
         <div name="case_grid" className="pt-[66px] pb-[77px]">
 
             <div name="container" className="w-full mx-auto max-w-[1142px]">
-
-
-
                 <div name="cases-grid__row" className="flex flex-wrap mx-[-5%]" >
-
-                    {cases.map((item, index) => {
-
-                        
+                    {cases.map((item, index) => {                    
+                        let id_img = "img_" + String(index)
+                        let id_title = "title_" + String(index)
                         return (
-                        <div
-                        onMouseEnter={() => {
-                            const ramdom_post_title_item = document.getElementById("title_cases-grid__item-title");
-                            const ramdon_post_img = document.getElementById("ramdon_post_img")
+                            <CardCase
 
-                            ramdom_post_title_item.classList.add("text-color_logo")
-                            ramdon_post_img.classList.add("scale-90", "rounded-[5px]")
-
-
-                        }}
-                        onMouseLeave={() => {
-                            const ramdom_post_title_item = document.getElementById("title_cases-grid__item-title");
-                            const ramdon_post_img = document.getElementById("ramdon_post_img")
-
-                            ramdom_post_title_item.classList.remove("text-color_logo")
-                            ramdon_post_img.classList.remove("scale-90", "rounded-[5px]")
-                        }}
-                        
-                        
-                        key={index} className="w-full max-w-[50%] p-[5%]" >
-
-                            <a name="cases-grid__item" className="" href="#">
-
-                                <div   name="cases-grid__item-img" className=" w-full h-[501px]  overflow-hidden rounded-[5px]">
-                                    <div>
-                                        <img id="ramdon_post_img"  src={item.image} alt="Equine is an NFT horse racing game. Race, breed, sell &amp; buy horses" loading="lazy"></img>
-                                        
-                                    </div>
-                                </div>  
-
-                                <ul id="cases-grid__item-tags" name="cases-grid__item-tags" className="flex flex-wrap leading-[24px] text-[16px] mt-[40px]">
-                                    {item.tags.map((tag, index) => {
-                                        return (<li key={index}>{tag}</li>)
-
-
-                                    })}
-
-                                </ul>
-
-                                <h3  id="title_cases-grid__item-title" className="text-[38px] leading-[48px] mt-[10px]">{item.title}</h3>
-
-
-                                <p name="desc cases-grid__item-desc" className="mt-25px leading-[28px] text-[18px] text-[#81817e]">{item.description}</p>
-                            </a>
-                        </div>)
+                                id_image= {id_img}
+                                id_title= {id_title}
+                                title = {item.title}
+                                image= {item.image}
+                                tags= {item.tags}
+                                description= {item.description}
+                            />
+                        )
 
                     })}
-
-
                 </div>
 
 
